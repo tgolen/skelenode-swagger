3d3d4c73d01b9906dc6b3c3ef5b4dfa9679bae35
Tim Golen <tgolen@appcelerator.com>
2014-12-27 14:41:52 -0700

    Initializing package
    Signed-off-by: Tim Golen <tgolen@appcelerator.com>

---
+++ readme.md
@@ -0,0 +1,74 @@
+# Skelenode Swagger
+This is a Skelenode component to provide common swagger utilities for standard responses and params. This can be used standalone with any swagger implementation.
+
+# Installation
+```
+npm install skelenode-swagger
+```
+
+# Usage
+```javascript
+var swr = require('skelenode-swagger/respond');
+
+// send a standard response from an express or restify route handler
+swr.respond({ msg: 'Hello World!' }, res);
+```
+
+The resulting response sent is always in the same form:
+```javascript
+{
+  success: true|false,
+  code: 200|404|ect.,
+  result: { ...whatever... }
+}
+```
+
+## Response Methods
+
+### success(result, res[, internalCode])
+Sends a 200 response with `result`.
+
+### custom(message, res[, internalCode, errorCode])
+By default, sends a 400 response with the given `message`. Pass an optional `errorCode` to override the default.
+
+### forbidden(res[, internalCode, message])
+Sends a 403 response. Pass an optional `message` to override the default message.
+
+### socketNotAllowed(res[, internalCode])
+Sends a 403 response saying that a socket request is not allowed.
+
+### xhrNotAllowed(res[, internalCode])
+Sends a 403 response saying that an XHR request is not allowed.
+
+### notFound(res[, internalCode])
+Sends a 404 response saying that the given resource was not found.
+
+### itemNotFound(name, res[, internalCode])
+Sends a 404 response saying that the resource named `name` was not found.
+
+### loginRequired(res[, internalCode])
+Sends a 401 response saying that a login is required.
+
+### loginInvalidated(res[, internalCode])
+Sends a 401 response saying that a login was invalidated.
+
+### paramRequired(field, res[, internalCode, errorCode])
+Sends a 400 response saying that a `field` is required.
+
+### paramInvalid(field, res[, internalCode])
+Sends a 400 response saying that a `field` is invalid.
+
+### serverError(res[, internalCode, errorCode])
+By default, sends a 500 response with the details of a server error. Pass an optional `errorCode` to override the default.
+
+### disabled(res[, internalCode])
+Sends a 500 response saying that feature is disabled.
+
+### localhostNotSupported(res[, internalCode])
+Sends a 500 response saying that localhost is not supported.
+
+### notImplemented(res[, internalCode])
+Sends a 501 response saying that API hasn't been implemented yet.
+
+# Contributing
+Open a pull request with plenty of well-written instructions on what you are submitting and why you are submitting it
---
+++ respond.js
@@ -0,0 +1,183 @@
+'use strict';
+
+/*
+ Public API.
+ */
+exports.success = success;
+exports.sendSuccess = sendSuccess;
+exports.error = {
+	custom: custom,
+	customSPrintF: customSPrintF,
+	socketNotAllowed: socketNotAllowed,
+	xhrNotAllowed: xhrNotAllowed,
+	notFound: notFound,
+	itemNotFound: itemNotFound,
+	accessDenied: forbidden,
+	forbidden: forbidden,
+	loginRequired: loginRequired,
+	loginInvalidated: loginInvalidated,
+	paramRequired: paramRequired,
+	paramInvalid: paramInvalid,
+	serverError: serverError,
+	disabled: disabled,
+	localhostNotSupported: localhostNotSupported,
+	notImplemented: notImplemented
+};
+
+/*
+ Implementation.
+ */
+
+/**
+ * Successful result.
+ * @param result Payload to return.
+ * @param res Optional express response to receive the payload.
+ * @param internalCode Optional internalCode to add to the payload.
+ */
+function success(result, res, internalCode) {
+	return sendSuccess(res, result, internalCode);
+}
+function custom(message, res, internalCode, errorCode) {
+	return sendStringError(res, message, errorCode || 400, internalCode);
+}
+function forbidden(res, internalCode, message) {
+	return sendStringError(res, message || 'You do not have access privileges to view this content.', 403, internalCode);
+}
+function socketNotAllowed(res, internalCode) {
+	return sendStringError(res, 'This API cannot be accessed via sockets; please use XHR.', 403, internalCode);
+}
+function xhrNotAllowed(res, internalCode) {
+	return sendStringError(res, 'This API cannot be accessed via XHR; please use sockets.', 403, internalCode);
+}
+function notFound(res, internalCode) {
+	return sendStringError(res, 'Resource Not Found', 404, internalCode);
+}
+function itemNotFound(name, res, internalCode) {
+	return sendSPrintFError(res, [ '"%s" Not Found', name ], 404, internalCode);
+}
+function loginRequired(res, internalCode) {
+	return sendStringError(res, 'Login Required', 401, internalCode);
+}
+function loginInvalidated(res, internalCode) {
+	return sendStringError(res, 'Login Invalidated', 401, internalCode);
+}
+function paramRequired(field, res, internalCode, errorCode) {
+	return sendSPrintFError(res, [ '"%s" is a required parameter.', field ], errorCode || 400, internalCode);
+}
+function paramInvalid(field, res, internalCode) {
+	return sendSPrintFError(res, [ '"%s" contained an invalid value.', field ], 400, internalCode);
+}
+function serverError(res, internalCode, errorCode) {
+	return sendStringError(res, 'The server has encountered an error. Please try again, or contact support.', errorCode || 500, internalCode);
+}
+function disabled(res, internalCode) {
+	return sendStringError(res, 'This feature is currently disabled.', 500, internalCode);
+}
+function localhostNotSupported(res, internalCode) {
+	return sendStringError(res, 'Localhost cannot support this request.', 500, internalCode, true);
+}
+function notImplemented(res, internalCode) {
+	return sendStringError(res, 'This API has not been implemented yet, but is reserved for future use.', 501, internalCode);
+}
+
+/*
+ Utility.
+ */
+
+/**
+ * Sends a successful response to the server.
+ * @param res Optional Express Response object.
+ * @param result The results that are successful and should be sent to the server. If this is a string, it'll be localized.
+ * @param internalCode An optional internal code to also send to the client. This is useful for programmatic use of the API so we can do non-string-based comparisons.
+ * @param enableStringLocalize Optional ability to enable localization of simple strings returned as success responses.
+ * @return {*} If no res is provided, the success JSON object will be returned; otherwise, the results of res.send are returned.
+ */
+function sendSuccess(res, result, internalCode) {
+	var obj = {
+			success: true,
+			code: res.statusCode,
+			result: result
+		};
+
+	if (internalCode !== undefined) {
+		obj.internalCode = internalCode;
+	}
+
+	if (!res) {
+		return obj;
+	}
+
+	try {
+		return res.send(obj);
+	}
+	catch(e) {
+		console.error('swagger.respond sendSuccess:', e, 'headersSent', res.headersSent);
+		if (res) {
+			res.end(JSON.stringify(obj));
+		}
+	}
+}
+
+/**
+ * Sends a SPrintF formatted error to the user. Otherwise, behaves the same as sendStringError.
+ * @param res Optional Express Response object.
+ * @param errorArray The first element should be a string; subsequent args are formatted in to the first string.
+ * @param code The HTTP Status code to send (like 404, 403, etc).
+ * @param internalCode An optional internal code to also send to the client. This is useful for programmatic use of the API so we can do non-string-based comparisons.
+ * @return {*} If no res is provided, the error JSON object will be returned; otherwise, the results of res.send are returned.
+ */
+function sendSPrintFError(res, errorArray, code, internalCode) {
+	var obj = { success: false };
+	obj[res ? 'description' : 'reason'] = errorArray;
+	return sendError(res, obj, code, internalCode);
+}
+
+/**
+ * Sends a string error to the user. Attempts to localize it.
+ * @param res Optional Express Response object.
+ * @param error The error to send to the user. If it's a string, it will be localized.
+ * @param code The HTTP Status code to send (like 404, 403, etc).
+ * @param internalCode An optional internal code to also send to the client. This is useful for programmatic use of the API so we can do non-string-based comparisons.
+ */
+function sendStringError(res, error, code, internalCode) {
+	var obj = { success: false };
+	obj[res ? 'description' : 'reason'] = error;
+	return sendError(res, obj, code, internalCode);
+}
+
+/**
+ * Sends an error down to the client. This is designed to be used by 'sendSPrintFError' and 'sendStringError'. Only use
+ * this if you know what you're up to, sir. :)
+ * @param res Optional Express Response object.
+ * @param obj The partially composed JSON response.
+ * @param code The HTTP Status code to send (like 404, 403, etc).
+ * @param internalCode An optional internal code to also send to the client. This is useful for programmatic use of the API so we can do non-string-based comparisons.
+ * @return {*} If no res is provided, the error JSON object will be returned; otherwise, the results of res.send are returned.
+ */
+function sendError(res, obj, code, internalCode) {
+	if (code) {
+		obj.code = code;
+	}
+	if (!res) {
+		if (internalCode !== undefined) {
+			obj.reason += ' { internalCode: ' + internalCode + ' }';
+		}
+		return obj;
+	}
+	if (internalCode !== undefined) {
+		obj.internalCode = internalCode;
+	}
+
+	try {
+		if (!res.headersSent) {
+			res.status(code);
+		}
+		return res.send(obj);
+	}
+	catch(e) {
+		console.error('swagger.respond sendError:', e, 'headersSent', res.headersSent);
+		if (res && res.status(code)) {
+			res.end(JSON.stringify(obj));
+		}
+	}
+}
